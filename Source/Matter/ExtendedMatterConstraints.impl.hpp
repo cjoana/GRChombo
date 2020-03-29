9c0714cd43bb3a624bba6ac4cec6b82dc7ca3638
cjoana <c.joana@uclouvain.be>
2020-03-29 22:02:35 +0200

    deleting dustfiles and other bad examples

--- Source/Matter/ExtendedMatterConstraints.impl.hpp
+++ Source/Matter/ExtendedMatterConstraints.impl.hpp
@@ -40,7 +40,10 @@ void MatterConstraints<matter_t>::compute(Cell<data_t> current_cell) const
 
     // Hamiltonain constraint
     out.Ham += -16.0 * M_PI * m_G_Newton * emtensor.rho;
-    out.Ham_rho = -16.0 * M_PI * m_G_Newton * emtensor.rho;
+    out.HamRel += pow(16.0 * M_PI * m_G_Newton * emtensor.rho, 2);
+    out.HamRel = (out.Ham * out.Ham) / out.HamRel ;
+    out.rho = emtensor.rho;
+    out.S = emtensor.S;
 
     // Momentum constraints
     FOR1(i) { out.Mom[i] += -8.0 * M_PI * m_G_Newton * emtensor.Si[i]; }
@@ -50,10 +53,15 @@ void MatterConstraints<matter_t>::compute(Cell<data_t> current_cell) const
     current_cell.store_vars(out.Mom, GRInterval<c_Mom1, c_Mom3>());
 
     // Extended output
-    current_cell.store_vars(out.Ham_ricci, c_Ham_ricci);
-    current_cell.store_vars(out.Ham_K, c_Ham_K);
-    current_cell.store_vars(out.Ham_trA2, c_Ham_trA2);
-    current_cell.store_vars(out.Ham_rho, c_Ham_rho);
+    // current_cell.store_vars(out.Ham_ricci, c_Ham_ricci);
+    // current_cell.store_vars(out.Ham_K, c_Ham_K);
+    // current_cell.store_vars(out.Ham_trA2, c_Ham_trA2);
+    // current_cell.store_vars(out.Ham_rho, c_Ham_rho);
+    current_cell.store_vars(out.ricci_scalar, c_ricci_scalar);
+    current_cell.store_vars(out.rho, c_rho);
+    current_cell.store_vars(out.trA2, c_trA2);
+    current_cell.store_vars(out.S, c_S);
+    current_cell.store_vars(out.HamRel, c_HamRel);
 }
 
 #endif /* MATTERCONSTRAINTS_IMPL_HPP_ */
