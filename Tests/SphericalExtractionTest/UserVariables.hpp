8ee4029cf03155fcaf7b2a4c6cd90d5cfa34db34
cjoana <c.joana@uclouvain.be>
2021-02-02 15:03:33 +0100

    update Tests to new GRCh

--- Tests/SphericalExtractionTest/UserVariables.hpp
+++ Tests/SphericalExtractionTest/UserVariables.hpp
@@ -6,22 +6,25 @@
 #ifndef USERVARIABLES_HPP
 #define USERVARIABLES_HPP
 
+#include "EmptyDiagnosticVariables.hpp"
 #include <array>
 #include <string>
 
+// assign enum to each variable
 enum
 {
-    c_chi,
-    c_lapse,
+    c_phi_Re,
+    c_phi_Im,
+
     NUM_VARS
 };
 
 namespace UserVariables
 {
-static const std::array<std::string, NUM_VARS> variable_names = {
-    "chi",
-    "lapse",
-};
+static const std::array<std::string, NUM_VARS> variable_names = {"phi_Re",
+                                                                 "phi_Im"};
 }
+
+#include "UserVariables.inc.hpp"
 
 #endif /* USERVARIABLES_HPP */
